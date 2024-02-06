e1b5efdeaea18b0569efbc6708f32a0bb9c31ad7
Artyom Tsanda <artyom.tsanda@tuhh.de>
2024-02-06 11:01:27 +0100

    packaging into deqmpi

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='deqmpi',
+    py_modules=['packagename'],
+    version='0.1.0',
+    description='A short description of the project.',
+    long_description=open('README.md').read().strip(),
+    author='A. Güngör, B. Askin, D. A. Soydan, C. B. Top, E. U. Saritas and T. Çukur',
+    license='MIT',
+)
